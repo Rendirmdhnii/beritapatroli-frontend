cf5b4535953e97b01366b9a14afecb647e28b1c3
Berita Patroli Dev <redaksi@beritapatroli.id>
2026-07-26 00:31:24 +0700

    feat: tambahkan konfigurasi permanent redirect 301 ke beritapatroli.co.id

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://beritapatroli.co.id/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
